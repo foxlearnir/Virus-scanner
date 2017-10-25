807aebe7748cdd04956c877f2f2fa79007778f04
foxlearnir <32983423+foxlearnir@users.noreply.github.com>
2017-10-25 04:52:40 +0330

    Add files via upload

---
+++ bot.php
@@ -0,0 +1,113 @@
+﻿<?php
+//site:foxlearn.ir
+//telegram:@ghanbari615
+//email:ghanbari615@gmail.com
+$update = json_decode(file_get_contents('php://input'));
+$token= "TOKEN"; //توکن شما
+define('API_KEY',$token);
+include("fnc.php");
+$message = $update->message;
+$from_id = $message->from->id;
+$chat_id = $message->chat->id;
+$text = $message->text;
+$textmessage = isset($update->message->text)?$update->message->text:'';
+$myphoto = $update->message->file_size;
+$chat_edit_id = $update->edited_message->chat->id;
+$edit = $update->edited_message;
+$message_edit_id = $update->edited_message->message_id;
+$cllchatid = $update->callback_query->message->chat->id;
+$data = $update->callback_query->data;
+$cllmsegid = $update->callback_query->message->message_id;
+$cllfor = $update->callback_query->from->id;
+if ($data == "ref")
+{
+	$data = file_get_contents("data/$cllchatid.txt");
+	if($data != "0")
+	{
+	$scn = scan($data,$cllchatid);
+	bot('editmessagetext',[
+    'chat_id'=>$cllchatid,
+	'message_id'=>$cllmsegid,
+    'text'=>"<code>$scn</code>", 
+    'parse_mode'=>'html',
+    'reply_markup'=>json_encode([
+    'inline_keyboard'=>[
+	[
+	['text'=>"🔄"."بروزرسانی",'callback_data'=>"ref"]
+	]
+    ]
+    ])
+    ]);
+	}
+	else
+	{
+	    bot('answercallbackquery',[
+        'callback_query_id'=>$update->callback_query->id,
+        'text'=>"✅اسکن فایل شما پایان یافته است.",
+        'show_alert'=>true
+       ]);
+	}
+}
+if($textmessage == "/start")
+{
+if(!is_file("data/$chat_id.txt"))
+{
+file_put_contents("data/$chat_id.txt","0");	
+}
+bot('sendMessage',[
+    'chat_id'=>$chat_id,
+    'text'=>"سلام,
+	لینک فایل مورد نظر را ارسال نمائید...", 
+    'reply_to_message_id'=>$message->message_id,
+    'parse_mode'=>'html',
+    'reply_markup'=>json_encode([
+    'inline_keyboard'=>[
+	[
+	['text'=>"ارتباط با ما",'url'=>"http://telegram.me/ghanbari615"]
+	]
+    ]
+    ])
+  ]);
+}
+else if (filter_var($textmessage, FILTER_VALIDATE_URL))
+{
+if(file_get_contents("data/$chat_id.txt") == "0")
+{
+file_put_contents("data/$chat_id.txt","$textmessage");
+$scn = scan($textmessage,$chat_id);
+bot('sendMessage',[
+    'chat_id'=>$chat_id,
+    'text'=>"<code>$scn</code>", 
+    'parse_mode'=>'html',
+    'reply_markup'=>json_encode([
+    'inline_keyboard'=>[
+	[
+	['text'=>"🔄"."بروزرسانی",'callback_data'=>"ref"]
+	]
+    ]
+    ])
+  ]);
+}
+else
+{
+	bot('sendMessage',[
+    'chat_id'=>$chat_id,
+    'text'=>"🔴شما یک فایل درحال اسکن دارید.....
+	تا زمانی که اسکن فایل قبلی به اتمام نرسد نمیتوانید فایل جدیدی ارسال کنید.
+	✅جهت بروزرسانی وضعیت اسکن فایل ارسالی قبلی بروی دکمه بروزرسانی کلیک کنید.", 
+    'parse_mode'=>'html'
+  ]);
+}
+}
+else
+{
+bot('sendMessage',[
+    'chat_id'=>$chat_id,
+    'text'=>"◀️"."آدرس وارد شده معتبر نمیباشد.نمونه آدرس معتبر:
+	http://foxlearn.ir/test.zip", 
+    'reply_to_message_id'=>$message->message_id,
+    'parse_mode'=>'html'
+  ]);
+}
+?>
+
---
+++ fnc.php
@@ -0,0 +1,101 @@
+﻿<?php
+function bot($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+ function sendmessage($chat_id, $text, $model){
+ bot('sendMessage',[
+ 'chat_id'=>$chat_id,
+ 'text'=>$text,
+ 'parse_mode'=>$mode
+ ]);
+ }
+ function sendphoto($chat_id, $photo, $captionl){
+ bot('sendphoto',[
+ 'chat_id'=>$chat_id,
+ 'photo'=>$photo,
+ 'caption'=>$caption,
+ ]);
+ }
+ function sendsticker($chat_id, $photo, $captionl){
+ bot('sendsticker',[
+ 'chat_id'=>$chat_id,
+ 'sticker'=>$photo,
+ 'caption'=>$caption,
+ ]);
+ }
+ function sendaudio($chat_id, $audio, $caption){
+ bot('sendaudio',[
+ 'chat_id'=>$chat_id,
+ 'audio'=>$audio,
+ 'caption'=>$caption,
+ ]);
+ }
+ function sendvoice($chat_id, $voice, $caption){
+ bot('sendvoice',[
+ 'chat_id'=>$chat_id,
+ 'voice'=>$voice,
+ 'caption'=>$caption,
+ ]);
+ }
+
+ function sendaction($chat_id, $action){
+ bot('sendchataction',[
+ 'chat_id'=>$chat_id,
+ 'action'=>$action
+ ]);
+ }
+    function get_data($url,$request=null){
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_USERAGENT,'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/32.0.1700.107 Chrome/32.0.1700.107 Safari/537.36');
+        curl_setopt($curl, CURLOPT_NOBODY, false);
+        //curl_setopt($curl, CURLOPT_HEADER, true);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        if(!empty($request)){
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($request));
+        }
+        $data = curl_exec($curl);
+        return $data;
+    }
+    function scan($url,$chat_id)
+    {
+    $url = urlencode($url);
+    $jsn=get_data("https://www.virustotal.com/ui/urls?url=$url",array("url"=>"null"));
+    $jsn=json_decode($jsn);
+    $id=$jsn->data->id;
+    $id=explode("-",$id);
+    $id=$id[1];
+    $urlscan = "https://www.virustotal.com/ui/search?query=$url&relationships[url]=network_location%2Clast_serving_ip_address&relationships[comment]=author%2Citem";
+    $jsn=json_decode(get_data($urlscan),true);
+    $str="";
+    if(isset($jsn['data'][0]['attributes']['last_analysis_results']['ESET']))
+    {
+	file_put_contents("data/$chat_id.txt","0");
+	$str="☑️نتیجه اسکن:\n";
+    $jsn=$jsn['data'][0]['attributes']['last_analysis_results'];
+    foreach($jsn As $key=>$value)
+    {
+        $str .= "🔶$key--->".$value['result']."\n";
+    }
+    }
+    else
+    {
+        $timenow = "Last Update:".time();
+        $str = "درحال اسکن..........\nشناسه فایل:\n$id\n$timenow";
+    }
+    return $str;
+    }
+?>
+
